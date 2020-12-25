9d6b6182df4bb882862eaa7b1363e259d7cf2237
senpai-gitkraken <rizkykurnia219@gmail.com>
2020-12-25 23:38:24 +0700

    menambahkan file controller Auth dan AuthModel

---
+++ app/Models/AuthModel.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class AuthModel extends Model
+{
+    protected $table = "user_developer";
+
+    public function register($data)
+    {
+        $query = $this->db->table($this->table)->insert($data);
+        return $query ? true : false;
+    }
+
+    public function cek_login($username)
+    {
+        $query = $this->table($this->table)
+            ->where('username_user', $username)
+            ->countAll();
+
+        if ($query > 0) {
+            $hasil = $this->table($this->table)
+                ->where('username_user', $username)
+                ->limit(1)
+                ->get()
+                ->getRowArray();
+        } else {
+            $hasil = array();
+        }
+
+        return $hasil;
+    }
+}
